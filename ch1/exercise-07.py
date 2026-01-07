a44a26e039bf8a125df3c1ba02a1a179c97c3482
Thien Vu <thien.vu@gmail.com>
2026-01-06 23:37:43 -0800

    Exercise 07

---
+++ ch1/exercise-07.py
@@ -0,0 +1,10 @@
+# Write a program that prints out your name and an interesting fact about
+# you. Here is a basic outline:
+#   def main():
+#       put your print statements here
+#   main()
+
+def main():
+    print("My name is Thien Vu")
+    print("Here is an interesting fact")
+main()
