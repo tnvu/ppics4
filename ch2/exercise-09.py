77e2655c6f5a8b2479d0e19933f8d2681e9ee84f
Thien Vu <thien.vu@gmail.com>
2026-01-08 21:48:42 -0800

    Ch 02 Exercise 09

---
+++ ch2/exercise-09.py
@@ -0,0 +1,5 @@
+# Write a program that converts temperatures from Fahrenheit to Celsius.
+
+fahr = float(input("Enter the Fahrenheit temp: "))
+celsius = 5/9 * (fahr - 32)
+print("Celsius temp is:", celsius)
