fe3f70a86fa95097a865a92754e17c8c97e36675
Thien Vu <thien.vu@gmail.com>
2026-01-08 21:51:58 -0800

    Ch02 Exercise 11

---
+++ ch2/exercise-11.py
@@ -0,0 +1,7 @@
+# Write a program to perform a unit conversion of your own choosing. Make
+# sure that the program prints an introduction that explains what it does.
+
+print("This program converts cm to inches")
+cm = float(input("Enter the number of cm: "))
+inches = cm / 2.54
+print("The number of inches is:", inches)
