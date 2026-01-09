4c66ad3712f8367b56e817353683bb024be6e96d
Thien Vu <thien.vu@gmail.com>
2026-01-08 21:54:06 -0800

    Ch02 Exercise 12

---
+++ ch2/exercise-12.py
@@ -0,0 +1,9 @@
+# Write a program that allows a student to find the average of three exam
+# scores entered by the user.
+
+print("This program finds the average of 3 exam scores.")
+score1 = float(input("Enter score 1: "))
+score2 = float(input("Enter score 2: "))
+score3 = float(input("Enter score 3: "))
+avg = (score1 + score2 + score3) / 3.0
+print("The average score is: ", avg)
