bc603f714d7174d9df509e63649531c6123f13f5
Thien Vu <thien.vu@gmail.com>
2026-01-08 21:50:16 -0800

    Ch02 Exercise 10

---
+++ ch2/exercise-10.py
@@ -0,0 +1,6 @@
+# Write a program that converts distances measured in kilometers to miles. One
+# kilometer is approximately 0.62 miles.
+
+km = float(input("Enter the number of kilometers: "))
+mi = 0.62 * km
+print("Number of miles: ", mi)
