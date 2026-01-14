d650b4ad07aaaff98a0e045d53170f66249a1433
Thien Vu <thien.vu@gmail.com>
2026-01-13 22:32:23 -0800

    Ch03 Exercise 16

---
+++ ch03/exercise-16.py
@@ -0,0 +1,18 @@
+# A Fibonacci sequence is a sequence of numbers where each successive
+# number is the sum of the previous two. The classic Fibonacci sequence
+# begins: 1, 1, 2, 3, 5, 8, 13, .... Write a program that computes the nth
+# Fibonacci number where n is a value input by the user. For example, if
+# n = 6, then the result is 8. Hints: Consider this an accumulator loop that
+# always tracks the current value and the previous value. Start these both at
+# 1 (the first two values of the sequence). Use simultaneous assignment to
+# set the next values of these two variables each time around the loop.
+
+def main():
+    n = int(input("Which Fibonacci number: "))
+    prev = 0
+    curr = 1
+    for _ in range(1, n):
+        prev, curr = curr, prev + curr
+    print("Fibonacci number: ", curr)
+
+main()
