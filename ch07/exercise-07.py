580158c1da048457ffd0b4d5ea18ad5a9c8a35c1
Thien Vu <thien.vu@gmail.com>
2026-02-05 23:29:29 -0800

    Ch07 Exercise 07

---
+++ ch07/exercise-07.py
@@ -0,0 +1,27 @@
+# Write a program that computes the fuel efficiency of a multi-leg journey.
+# The program will first prompt for the starting odometer reading and then
+# get information about a series of legs. For each leg, the user enters the
+# current odometer reading and the amount of gas used. The user signals
+# the end of the trip with a blank line. The program should print out the
+# miles per gallon achieved on each leg and the total MPG for the trip.
+
+def main():
+    odo_start = float(input("Enter starting odometer: "))
+    total_mileage = 0.0
+    total_gas_used = 0.0
+    while True:
+        odo_current = input("Enter current odometer: ")
+        if odo_current == "": break
+        gas_used = input("Enter amount of gas used: ")
+        if gas_used == "": break
+        
+        odo_current = float(odo_current)
+        gas_used = float(gas_used)
+        mileage = odo_current - odo_start
+        print("Leg mpg =", mileage / gas_used)
+
+        total_mileage = total_mileage + mileage
+        total_gas_used = total_gas_used + gas_used
+    print("Overall mpg =", total_mileage / total_gas_used)
+
+main()
