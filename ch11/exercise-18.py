20eb98c5f59e94890eb150729ee4edd389652435
Thien Vu <thien.vu@gmail.com>
2026-02-16 22:57:12 -0800

    Ch11 Exercise 18 - TODO

---
+++ ch11/exercise-18.py
@@ -0,0 +1,11 @@
+# (Advanced) Here is a puzzle problem that can be solved with either some
+# fancy analytic geometry (calculus) or a (relatively) simple simulation.
+# Suppose you are located at the exact center of a cube. If you could look
+# all around you in every direction, each wall of the cube would occupy %
+# of your field of vision. Suppose you move toward one of the walls so that
+# you are now halfway between it and the center of the cube. What fraction
+# of your field of vision is now taken up by the closest wall? Hint: Use a
+# Monte Carlo simulation that repeatedly “looks” in a random direction and
+# counts how many times it sees the wall.
+
+# TODO(thien): I need to think about this problem a lot more
